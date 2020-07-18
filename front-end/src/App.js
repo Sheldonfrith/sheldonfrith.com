a4aedbc9a989bb5c7e9a5f89636cb70f4782cade
Sheldon Frith <frith1993@yahoo.ca>
2020-07-18 13:27:33 -0400

    discard these changes, app is broken

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,28 +8,30 @@ import Header from '@material-ui/core/Header';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles((theme) => ({
-  mainGrid: {
-    marginTop: theme.spacing(3),
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   mainGrid: {
+//     marginTop: theme.spacing(3),
+//   },
+// }));
 
 function App() {
-  const classes = useStyles();
+  // const classes = useStyles();
   return (
-    <CssBaseline>
+    <div className="App">
+      <header className="App-header"><p>testing</p></header>
+    {/* <CssBaseline>
     <ThemeProvider>
-    <Container className="App">
-      <Header title="sheldonfrith.com" className="App-header">
-      </Header>
-      <main>
+      <header title="sheldonfrith.com" className="App-header">TestHeader
+      </header>
+      {/* <main>
         <Grid container spacing={4}>
           <Button>testing button</Button>
         </Grid>
-      </main>
-    </Container>
-    </ThemeProvider>
-    </CssBaseline>
+      </main> */}
+    {/* </ThemeProvider>
+    </CssBaseline> */} */}
+    </div>
+
   );
 }
 
