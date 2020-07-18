5e9dcae44482f5c25b38b5399c7b8e7ae4c95590
Sheldon Frith <frith1993@yahoo.ca>
2020-07-18 13:12:58 -0400

    testing out material ui implementation

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,35 @@
+import { ThemeProvider } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from '@material-ui/core/Container';
+import Header from '@material-ui/core/Header';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CssBaseline>
+    <ThemeProvider>
+    <Container className="App">
+      <Header title="sheldonfrith.com" className="App-header">
+      </Header>
+      <main>
+        <Grid container spacing={4}>
+          <Button>testing button</Button>
+        </Grid>
+      </main>
+    </Container>
+    </ThemeProvider>
+    </CssBaseline>
   );
 }
 
