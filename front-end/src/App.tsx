94846870310b30b6f46465b863b11e243bf1d109
Sheldon Frith <frith1993@yahoo.ca>
2020-07-19 01:44:08 -0400

    styling the site

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,105 +1,185 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import injectSheet from 'react-jss';
+
+
+
 import Checkbox from '@material-ui/core/Checkbox';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme, createStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
-import tileData from './tileData';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import SchoolIcon from '@material-ui/icons/School';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import HomeIcon from '@material-ui/icons/Home';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from '@material-ui/icons/Instagram'; 
+import GitHubIcon from '@material-ui/icons/GitHub';
+import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAlt';
+
+const useStyles = makeStyles((theme) => createStyles({
   root: {
     display: 'flex',
     flexDirection: 'column',
+    justifyContent: 'center',
     minHeight: '100vh',
   },
+  pageNarrower: {
+  },
   header: {
-    backgroundColor: theme.palette.type ==='light'? theme.palette.primary.light: theme.palette.primary.dark,
-    marginTop: theme.spacing(0),
-    marginBottom: theme.spacing(2),
+    backgroundColor: theme.palette.type==='light'? theme.palette.primary.light: theme.palette.primary.dark,
+    margin: 0,
+    padding: 0,
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    position: 'sticky',
+    zIndex: 1000,
+    top: 0,
+  },
+  gridListWrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    marginBottom: 0,
+    marginTop: 0,
+   backgroundColor: theme.palette.type === 'light'? theme.palette.background.paper: theme.palette.background.paper,
+  },
+  socialMediaIcon: {
+    margin: 20,
   },
   gridList: {
-
+    width: '100%',
+    minHeight: 500,
+    justifyContent: 'center',
+    padding: 2,
   },
-  gridItem1: {
-    height: 150,
-    width: 200
+  gridListTile:{
+    border: '100px',
+    height: '100%',
+    shadow: "1px 1px black",
   },
-  gridItem2: {
-    height: 300,
-    width: 100,
-    backgroundColor: theme.palette.type === 'light'? theme.palette.warning.light : theme.palette.warning.dark,
+  gridListTileBar:{
+    background: "rgba(75,110,115,0.75)",
+    textShadow: "1px 1px black",
   },
-  footer: {
-    padding: theme.spacing(3, 2),
-    marginTop: 'auto',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+  gridListTitleTile: {
   },
-}));
+  gridListTitle: {
+  },
+  bottomNavigation: {
+    width: '100%',
+    backgroundColor: 'inherit',
+  },
+  bottomNavigationWrapper:{
+    backgroundColor: theme.palette.type === 'light'? theme.palette.secondary.light: theme.palette.secondary.dark,
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    position:'sticky',
+    bottom:0,
+  },
+  bottomNavigationAction:{
+    color: theme.palette.text.disabled,
+    '&.Mui-selected': {
+      color: theme.palette.text.primary,
+    },
+  },
+  hireMeButton: {
+    backgroundColor: theme.palette.secondary.main,
+    margin: 10,
+    color: theme.palette.common.white,
+  },
+  
+})
+);
 
 function App() {
   const classes = useStyles();
+  const theme = useTheme();
   return (
     
     <div className={'App '+classes.root}>
       <CssBaseline />
-
+      <Container component="div" maxWidth="sm" className={classes.pageNarrower}>
       <Container component="header" maxWidth="sm" className={'App-header '+classes.header}>
-        <Typography variant="h2" component="h1" gutterBottom>
+        <Typography variant="h3" component="h1">
           Sheldon Frith
         </Typography>
+        {/* social media links go here */}
+          <LinkedInIcon className={classes.socialMediaIcon}/>
+          <InstagramIcon className={classes.socialMediaIcon}/>
+          <GitHubIcon className={classes.socialMediaIcon}/>
       </Container>
+      <div className={classes.gridListWrapper}>
+      <GridList cellHeight={250} className={classes.gridList} cols={3} spacing={4}>
+        {/* <GridListTile key="Subheader" cols={3} style={{height:'auto'}} className={classes.gridListTitleTile}>
+          <ListSubheader component="div" className={classes.gridListTitle}>Projects</ListSubheader>
+        </GridListTile> */}
 
-      <GridList cellHeight={180} container justify="center" className={classes.gridList} spacing={2}>
-        <GridListTitle key="Subheader" cols={2} style={{height:'auto'}}>
-          <ListSubheader component="div">Projects</ListSubheader>
-        </GridListTitle>
-        <GridListTile key="t-y-p-e_tile">
-          <img src={} alt={'t-y-p-e.com image'}/>
+        <GridListTile key="t-y-p-e_tile" cols={2} className={classes.gridListTile}>
+          <img src={'./images/t-y-p-e_code.png'} alt={'t-y-p-e.com'}/>
           <GridListTileBar
             title={'T-Y-P-E'}
             subtitle={'Customizable typing speed training for programmers.'}
+            className={classes.gridListTileBar}
             />
         </GridListTile>
-        <GridListTile key="ebook_tile">
-          <img src={} alt="image of letter to a vegetarian nation ebook"/>
+
+        <GridListTile key="ebook_tile" cols={1} className={classes.gridListTile}>
+          <img src={'./images/ltavn.png'} alt="letter to a vegetarian nation ebook"/>
           <GridListTileBar
             title={'Letter To A Vegetarian Nation'}
             subtitle={'Regenerative agriculture book.'}
+            className={classes.gridListTileBar}
             />
         </GridListTile>
-        <GridListTile key="wordpress_websites">
-          <img src={} alt="image of my wordpress websites"/>
+
+        <GridListTile key="wordpress_websites" cols={1} className={classes.gridListTile}>
+          <img src={'./images/joinedwebsites.jpeg'} alt="image of my wordpress websites"/>
           <GridListTileBar 
           title={'Simple Wordpress Websites'}
           subtitle={'From my pre-webdev days.'}
+          className={classes.gridListTileBar}
           />
         </GridListTile>
-        <GridListTile key="current_projects">
-          <img src={} alt="image of the projects I'm currently working on"/>
+
+        <GridListTile key="current_projects" cols={2} className={classes.gridListTile}>
+          <img src={'./images/modelviewer.png'} alt="image of the projects I'm currently working on"/>
           <GridListTileBar
           title={'Ongoing Dev Projects'}
           subtitle={'This is what I\'m currently working on.'}
+          className={classes.gridListTileBar}
           />
         </GridListTile>
       </GridList>
-
-      <footer className = {classes.footer}>
-        <Container maxWidth="sm">
-          <Typography variant="body1">Footer area</Typography>
-        </Container>
-      </footer>
-      
+      </div>
+      <Container component="div" maxWidth="sm" className={classes.bottomNavigationWrapper}>
+      <BottomNavigation
+        value={0}
+        onChange={(event, newValue) => {console.log(newValue);}}
+        className={classes.bottomNavigation}
+        >
+          <BottomNavigationAction showLabel label="My Projects" icon={<HomeIcon/>} className={classes.bottomNavigationAction}/>
+          <BottomNavigationAction showLabel label="Links" icon={<ListAltIcon/>}  className={classes.bottomNavigationAction}/>
+          {/* <BottomNavigationAction showLabel label="Hire Me" icon={<SentimentSatisfiedAltIcon/>} className={classes.hireMeButton}/> */}
+          <Button variant="contained" className={classes.hireMeButton}>Hire Me</Button>
+          <BottomNavigationAction showLabel label="My CV" icon={<SchoolIcon/>} className={classes.bottomNavigationAction}/>
+        <BottomNavigationAction showLabel label="Contact Me" icon={<ContactMailIcon/>} className={classes.bottomNavigationAction}/>
+        </BottomNavigation>
+      </Container>
+      </Container>
     </div>
   );
 }
